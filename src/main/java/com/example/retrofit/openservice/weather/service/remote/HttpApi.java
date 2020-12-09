fdf35ee7772892c84ba196997c8bb807831f89f2
wangyuxi <Wangyuxi@hollysys.net>
2020-12-09 18:45:48 +0800

    1. 演示重试功能

--- src/main/java/com/example/retrofit/openservice/weather/service/remote/HttpApi.java
+++ src/main/java/com/example/retrofit/openservice/weather/service/remote/HttpApi.java
@@ -8,6 +8,7 @@ import com.example.retrofit.openservice.weather.service.remote.fallback.HttpDegr
 import com.github.lianjiatech.retrofit.spring.boot.annotation.Intercept;
 import com.github.lianjiatech.retrofit.spring.boot.annotation.OkHttpClientBuilder;
 import com.github.lianjiatech.retrofit.spring.boot.annotation.RetrofitClient;
+import com.github.lianjiatech.retrofit.spring.boot.retry.Retry;
 import okhttp3.OkHttpClient;
 import retrofit2.http.GET;
 import retrofit2.http.QueryMap;
@@ -32,6 +33,16 @@ import java.util.concurrent.TimeUnit;
 /*2.2.2  @Intercept支持多拦截器配置*/
 @Intercept(handler = TimeStampInterceptor.class, include = {"/api/**"})
 @Intercept(handler = AnotherTimeStampInterceptor.class, include = {"/api/**"})
+
+/*请求重试
+retrofit-spring-boot-starter支持请求重试功能，只需要在接口或者方法上加上@Retry注解即可。
+@Retry支持重试次数maxRetries、重试时间间隔intervalMs以及重试规则retryRules配置。重试规则支持三种配置：
+
+RESPONSE_STATUS_NOT_2XX：响应状态码不是2xx时执行重试；
+OCCUR_IO_EXCEPTION：发生IO异常时执行重试；
+OCCUR_EXCEPTION：发生任意异常时执行重试；
+默认响应状态码不是2xx或者发生IO异常时自动进行重试。需要的话，你也可以继承BaseRetryInterceptor实现自己的请求重试拦截器，然后将其配置上去。*/
+@Retry
 public interface HttpApi {
 
     /**
