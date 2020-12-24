c4b8c4a4ece58c479aac04ebfeeba453b8813352
wangyuxi <yuxi007008@163.com>
2020-12-24 23:55:57 +0800

    1. 去除无用的代码

--- retrofit-sample/src/main/java/com/example/App.java
+++ retrofit-sample/src/main/java/com/example/App.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import com.github.lianjiatech.retrofit.spring.boot.annotation.RetrofitScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
